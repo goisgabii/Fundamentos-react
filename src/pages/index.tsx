310e351ba6ffdaecfaf4922d8d2065b16e55ef28
MoisesCesul <moises.2023014223@alunocesul.com>
2025-07-29 21:22:00 -0300

    chore: biome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-export default function Home(){
-    return(
-        <div>
-            Hello World!
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
 }
