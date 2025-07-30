24a4067bc28547b163901b850ecc54a0b69d27fa
MoisesCesul <moises.2023014223@alunocesul.com>
2025-07-29 21:53:00 -0300

    feature: add background-color lighy gray

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "@chakra-ui/react";
+
+export const themeConfig = defineConfig({
+  globalCss:{
+    "html, body" : {
+      backgroundColor:"gray.50"
+    }
+  },
+  theme : {
+    tokens : {
+      colors : {},
+    },
+  },
+})
+
