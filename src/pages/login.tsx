a4d4d858fd0a3d8845efa629a051bf165ac31fe9
goisgabii <gabriela.2023014224@alunocesul.com>
2025-08-05 22:21:22 -0300

    aula05-08

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,39 +1,62 @@
-import {Button, Link as ChakraLink, Flex, Heading, HStack, Image, Input, Stack, Text, VStack } from "@chakra-ui/react";
+import {Button, Link as ChakraLink, Field, Flex, Heading, HStack, Image, Input, Stack, Text, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
-<Image w={500} h={500} src="/assets/logo.gif" alt="Login image" />
+<Image src="/assets/login-image.gif"  />
+import { Checkbox } from "@/components/ui/checkbox";
+import { PasswordInput } from "@/components/ui/password-input";
+import { useForm } from "react-hook-form";
+import z, { email } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const signInFormSchema = z.object({
+  email: z.email("O e-mail é obrigatório").nonempty("Digite um e-mail válido"),
+  password: z.string().nonempty("A senha é obrigatória").min(8,"A senha deve ter pelo menos 8 caracteres"),
+});
 
-
-import { Checkbox} from "@/components/ui/checkbox";
-
-
-
+type SignInFormData = z.infer<typeof signInFormSchema>;
 
 export default function Login() {
+
+    const { register, handleSubmit, formState: {errors} } = useForm({
+      resolver: zodResolver(signInFormSchema)
+    });
+
     return (
         <Flex w="100vw" h="100vh">
-          <Flex w="50%" bg="#2C73EB" align="center" justify="center">
-            <Image w={500} h={500} src="/assets/logo.gif" alt="Login image" />
+          <Flex w="50%" bg="#2C73EB" align={"center"} justify={"center"}>
+            <Image w={500} h={500} src="/assets/logo.gif" alt="Login image"  />
           </Flex>
-
-          <VStack w="50%" bg="green.400" justify="center">
+          <VStack w="50%" justify="center">
             <Stack>
-              <Heading as="h1">Login</Heading> 
+              <Heading as="h1" color="black" fontSize="3xl" fontWeight="bold">Login</Heading>
 
-              <Text>Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
+              <Text color="gray.400" fontWeight="normal" fontSize="lg">Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
 
-              <Input />
-              <Input />
+            <VStack align="flex-start" gap={6} mt={10}>
+              <Field.Root invalid={!!errors.email}>
+              <Field.Label color="gray.500" fontSize="md">
+                Email <Field.RequiredIndicator />
+              </Field.Label>
+              <Input type="email" colorPalette="blue" h={16} borderRadius="md" color="black" {...register("email")} />
 
-              <Checkbox>
-                Lembre-me
-              </Checkbox>
+            </Field.Root>
+             <Field.Root invalid={!!errors.password}>
+              <Field.Label color="gray.500" fontSize="md">
+                Senha <Field.RequiredIndicator />
+              </Field.Label>
+               <PasswordInput colorPalette="blue" h="16" borderRadius="md" color="black" />
+               <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+            </Field.Root>
+            
+            <Checkbox colorPalette="blue" color="gray.500" fontSize="md" fontWeight="medium">
+              Lembre-me
+            </Checkbox>
 
-              <Button>Entrar</Button>
+            <Button w="full" h="16" colorPalette="blue" borderRadius="md" fontSize="md" fontWeight="medium">Entrar</Button>
+          </VStack>
 
-              <HStack>
-                <Text>Não possui uma conta?</Text>
-                <ChakraLink asChild>
+             <HStack justify="center" gap={1} mt={10}>
+                <Text color="gray.500" fontSize="md" fontWeight="medium">Não possui uma conta?</Text>
+                <ChakraLink asChild color="gray.500">
                   <NextLink href="/sign-up">Cadastre-se</NextLink>
                 </ChakraLink>
 
