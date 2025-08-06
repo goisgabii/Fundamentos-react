3fdbb25cb3581c470cb0c9e486abdc6c6c007997
goisgabii <gabriela.2023014224@alunocesul.com>
2025-08-05 22:06:16 -0300

    aula0508

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import { Flex } from "@chakra-ui/react";
-
-
-export default function Home() {
-  return (
-  <Flex w="100vw" h="100vh">
-
-
-  </Flex>
-  )
+export default  function Home() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,47 @@
-import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
-import loginImage from "../../public/assets/logo.gif"
+import {Button, Link as ChakraLink, Flex, Heading, HStack, Image, Input, Stack, Text, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
+<Image w={500} h={500} src="/assets/logo.gif" alt="Login image" />
+
+
+
+import { Checkbox} from "@/components/ui/checkbox";
+
+
+
 
 export default function Login() {
-  return (
-     <Flex w="100vw" h="100vh">
-       <Flex w="50%" bg="#2C73EB" align="center" justify="center">
+    return (
+        <Flex w="100vw" h="100vh">
+          <Flex w="50%" bg="#2C73EB" align="center" justify="center">
+            <Image w={500} h={500} src="/assets/logo.gif" alt="Login image" />
+          </Flex>
 
-         <Image w={500} h={500} src={loginImage.src}/>
-       </Flex>
-       <VStack w="50%" bg={"black"}  >
-          <Heading as={"h1"}>Account Login</Heading>
-          <Text>If you are already a member you can login with your email address and password.</Text>
-       </VStack>
-     </Flex>
-  )
+          <VStack w="50%" bg="green.400" justify="center">
+            <Stack>
+              <Heading as="h1">Login</Heading> 
+
+              <Text>Se você já é membro, você pode fazer login com seu endereço de e-mail e senha.</Text>
+
+              <Input />
+              <Input />
+
+              <Checkbox>
+                Lembre-me
+              </Checkbox>
+
+              <Button>Entrar</Button>
+
+              <HStack>
+                <Text>Não possui uma conta?</Text>
+                <ChakraLink asChild>
+                  <NextLink href="/sign-up">Cadastre-se</NextLink>
+                </ChakraLink>
+
+              </HStack>
+
+
+            </Stack>
+          </VStack>
+        </Flex>
+    )
 }
